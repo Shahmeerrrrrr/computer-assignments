7fe4c8d52e4d18b11a72010fd99100d6c3fddf4d
Shahmeer <shahmeerraheel786@gmail.com>
2025-04-21 17:37:45 -0400

    Add lab6ii.py: Calculate the sum of three numbers

---
+++ lab6ii.py
@@ -0,0 +1,19 @@
+def getSum(num1, num2, num3):
+    return num1 + num2 + num3
+
+num1 = float(input("Enter ur first number: "))
+num2 = float(input("Enter ur second number: "))
+num3 = float(input("Enter ur third number: "))
+
+result = getSum(num1, num2, num3)
+
+print("The sum is:", result)
+
+
+# Output:
+# PS C:\Users\HP\OneDrive\Desktop\computer-assignments> & C:/Python313/python.exe c:/Users/HP/OneDrive/Desktop/computer-assignments/lab6ii.py
+# Enter ur first number: 6
+# Enter ur second number: 7
+# Enter ur third number: 8
+# The sum is: 21.0
+# PS C:\Users\HP\OneDrive\Desktop\computer-assignments> 
