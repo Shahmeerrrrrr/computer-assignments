ae45eefd55ffb8ff01e89d33ee5d038b1a9db761
Shahmeer <shahmeerraheel786@gmail.com>
2025-06-02 19:36:23 -0400

    Add lab6i.py: Perform mathematical operations on a floating-point number

---
+++ lab6i.py
@@ -0,0 +1,21 @@
+import math
+
+num = float(input("Enter a Floating number: "))
+
+sqrt = math.sqrt(num)
+
+power = num ** 4
+
+floor = math.floor(num)
+
+print(f"Square root of the number is: {sqrt}")
+print(f"Fourth power of the number is: {power}")
+print(f"Floor of the number is: {floor}")
+
+# Output:
+# PS C:\Users\HP\OneDrive\Desktop\computer-assignments> & C:/Python313/python.exe c:/Users/HP/OneDrive/Desktop/computer-assignments/lab6i.py
+# Enter a Floating number: 6.3
+# Square root of the number is: 2.5099800796022267
+# Fourth power of the number is: 1575.2960999999998
+# Floor of the number is: 6
+# PS C:\Users\HP\OneDrive\Desktop\computer-assignments> 
